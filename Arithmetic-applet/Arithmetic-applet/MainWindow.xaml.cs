ae2279506dfa56937eeee100715417847c0ebde1
rlxing <17623593008@163.com>
2020-10-23 23:29:43 +0800

    随机出题部分改写为类的方式

--- Arithmetic-applet/Arithmetic-applet/MainWindow.xaml.cs
+++ Arithmetic-applet/Arithmetic-applet/MainWindow.xaml.cs
@@ -18,7 +18,6 @@ namespace Arithmetic_applet
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    
     public class publicValue
     {
         public static String formula;//式子
@@ -52,52 +51,32 @@ namespace Arithmetic_applet
 
         private void set_question(object sender, RoutedEventArgs e)
         {
-            int firstNumber, secondNumber;
-            int symbols;
-
-            Random random = new Random();
-
-            while (true)
+            Arithmetic arithmetic = new Arithmetic();
+            int[] formula = arithmetic.SetFormula();
+            switch (formula[1])
             {
-                symbols = random.Next(0, 4);//0:+  1:-  2:*  3:/
-                firstNumber = random.Next(0, 11);
-                secondNumber = random.Next(0, 11);
-
-                //减法情况下，前一个数不能小于后面一个数
-                if (symbols == 1 && firstNumber < secondNumber)
-                    continue;
-                //除法情况下，不能出现小数，除数不能为0， 分子不能大于分母
-                else if (symbols == 3 && ( firstNumber < secondNumber || secondNumber == 0 || firstNumber % secondNumber != 0))
-                    continue;
-                else
-                {
-                    switch (symbols)
-                    {
-                        case 0://+
-                            publicValue.questionAnswer = firstNumber + secondNumber;
-                            publicValue.formula = firstNumber.ToString() + "+" + secondNumber.ToString();
-                            questionBox.Content = publicValue.formula;
-                            break;
-                        case 1://-
-                            publicValue.questionAnswer = firstNumber - secondNumber;
-                            publicValue.formula = firstNumber.ToString() + "-" + secondNumber.ToString();
-                            questionBox.Content = publicValue.formula;
-                            break;
-                        case 2://*
-                            publicValue.questionAnswer = firstNumber * secondNumber;
-                            publicValue.formula = firstNumber.ToString() + "×" + secondNumber.ToString();
-                            questionBox.Content = publicValue.formula;
-                            break;
-                        case 3:///
-                            publicValue.questionAnswer = firstNumber / secondNumber;
-                            publicValue.formula = firstNumber.ToString() + "÷" + secondNumber.ToString();
-                            questionBox.Content = publicValue.formula;
-                            break;
-                        default:
-                            break;
-                    }
+                case 0://+
+                    publicValue.questionAnswer = formula[3];
+                    publicValue.formula = formula[0].ToString() + " + " + formula[2].ToString();
+                    questionBox.Content = publicValue.formula;
                     break;
-                }
+                case 1://-
+                    publicValue.questionAnswer = formula[3];
+                    publicValue.formula = formula[0].ToString() + " - " + formula[2].ToString();
+                    questionBox.Content = publicValue.formula;
+                    break;
+                case 2://*
+                    publicValue.questionAnswer = formula[3];
+                    publicValue.formula = formula[0].ToString() + " × " + formula[2].ToString();
+                    questionBox.Content = publicValue.formula;
+                    break;
+                case 3:///
+                    publicValue.questionAnswer = formula[3];
+                    publicValue.formula = formula[0].ToString() + " ÷ " + formula[2].ToString();
+                    questionBox.Content = publicValue.formula;
+                    break;
+                default:
+                    break;
             }
         }
 
@@ -105,6 +84,7 @@ namespace Arithmetic_applet
         {
             String inListContent;//写入列表框的内容
             String content = answerBox.Text;//获取用户输入内容
+            Arithmetic arithmetic = new Arithmetic();
             if (content.Length == 0)//检测用户输入是否为空
             {
                 MessageBox.Show(mainWindow, "答案不能为空");
@@ -114,9 +94,9 @@ namespace Arithmetic_applet
             {
                 int userAnswer = Int32.Parse(content);
 
-                if (publicValue.questionAnswer == userAnswer)
+                if (arithmetic.CheckAnswer(publicValue.questionAnswer, userAnswer))
                 {
-                    inListContent = publicValue.formula + "=" + userAnswer.ToString() + "正确";
+                    inListContent = publicValue.formula + " = " + userAnswer.ToString() + " 正确";
                     answerList.Items.Insert(0, inListContent);
                     //总做题数和正确题数计数
                     publicValue.sumDoCount++;
@@ -126,7 +106,7 @@ namespace Arithmetic_applet
                 }
                 else
                 {
-                    inListContent = publicValue.formula + "=" + userAnswer.ToString() + "错误\t" + "正确答案：" + publicValue.questionAnswer.ToString();
+                    inListContent = publicValue.formula + " = " + userAnswer.ToString() + " 错误\t" + "正确答案：" + publicValue.questionAnswer.ToString();
                     answerList.Items.Insert(0, inListContent);
                     //总做题数和错误题数计数
                     publicValue.sumDoCount++;
@@ -163,6 +143,66 @@ namespace Arithmetic_applet
             beginButton.IsEnabled = true;
             confirmButton.IsEnabled = false;
             submitButton.IsEnabled = false;
+        }
+    }
+}
+public class Arithmetic{
+    /*private int rightCount = 0;//正确题数
+    private int sumCount = 0;//总题数*/
+    public int[] SetFormula()//设置式子
+    {
+        int[] formula = new int[4];//保存式子的数组
+        int firstNumber, secondNumber;
+        int symbols;
+
+        Random random = new Random();
+
+        while (true)
+        {
+            symbols = random.Next(0, 4);//0:+  1:-  2:*  3:/
+            firstNumber = random.Next(0, 11);
+            secondNumber = random.Next(0, 11);
+
+            if (symbols == 1 && firstNumber < secondNumber)
+                continue;//减法情况下，前一个数不能小于后面一个数
+            else if (symbols == 3 && (firstNumber < secondNumber || secondNumber == 0 || firstNumber % secondNumber != 0))
+                continue;//除法情况下，不能出现小数，除数不能为0， 分子不能大于分母
+            else
+            {
+                formula[0] = firstNumber;
+                formula[1] = symbols;
+                formula[2] = secondNumber;
+                switch (symbols)
+                {
+                    case 0://+
+                        formula[3] = firstNumber + secondNumber;
+                        break;
+                    case 1://-
+                        formula[3] = firstNumber - secondNumber;
+                        break;
+                    case 2://*
+                        formula[3] = firstNumber * secondNumber;
+                        break;
+                    case 3:///
+                        formula[3] = firstNumber / secondNumber;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            }
+        }
+        return formula;
+    }
+    public bool CheckAnswer(int a, int b)//进行答案的判断
+    {
+        if(a == b)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
